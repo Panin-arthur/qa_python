2ec8290aaf7ba2a889e2bd8e8cfecb305a35d294
Artur Panin <artur.panin@x5.ru>
2023-09-11 00:43:53 +0300

    New tests by PAO

--- tests.py
+++ tests.py
@@ -1,24 +1,71 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-class TestBooksCollector:
+import unittest
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+class TestBooksCollector(unittest.TestCase):
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def setUp(self):
+        self.collector = BooksCollector()
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_add_new_book(self):
+        self.collector.add_new_book("Книга1")
+        self.assertTrue("Книга1" in self.collector.books_genre)
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_duplicate(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.add_new_book("Книга1")  # Попытка добавить дубликат
+        self.assertEqual(len(self.collector.books_genre), 1)  # Должна быть только одна запись
+
+    def test_add_new_book_invalid_name(self):
+        self.collector.add_new_book("Книга с очень длинным названием, которое превышает 40 символов")  # Слишком длинное название
+        self.assertFalse("Книга с очень длинным названием, которое превышает 40 символов" in self.collector.books_genre)
+
+    def test_set_book_genre(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.set_book_genre("Книга1", "Фантастика")
+        self.assertEqual(self.collector.get_book_genre("Книга1"), "Фантастика")
+
+    def test_get_books_with_specific_genre(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.set_book_genre("Книга1", "Фантастика")
+        self.collector.add_new_book("Книга2")
+        self.collector.set_book_genre("Книга2", "Детективы")
+        books = self.collector.get_books_with_specific_genre("Фантастика")
+        self.assertEqual(len(books), 1)
+        self.assertTrue("Книга1" in books)
+
+    def test_get_books_for_children(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.set_book_genre("Книга1", "Фантастика")
+        self.collector.add_new_book("Книга2")
+        self.collector.set_book_genre("Книга2", "Детективы")
+        self.collector.add_new_book("Книга3")
+        self.collector.set_book_genre("Книга3", "Ужасы")
+        books = self.collector.get_books_for_children()
+        self.assertEqual(len(books), 1)  # Только "Книга1" подходит для детей
+        self.assertTrue("Книга1" in books)
+
+    def test_add_book_in_favorites(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.add_book_in_favorites("Книга1")
+        self.assertEqual(len(self.collector.get_list_of_favorites_books()), 1)
+
+    def test_add_book_in_favorites_duplicate(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.add_book_in_favorites("Книга1")
+        self.collector.add_book_in_favorites("Книга1")  # Попытка добавить дубликат
+        self.assertEqual(len(self.collector.get_list_of_favorites_books()), 1)  # Должна быть только одна избранная книга
+
+    def test_delete_book_from_favorites(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.add_book_in_favorites("Книга1")
+        self.collector.delete_book_from_favorites("Книга1")
+        self.assertEqual(len(self.collector.get_list_of_favorites_books()), 0)
+
+    def test_get_books_genre(self):
+        self.collector.add_new_book("Книга1")
+        self.collector.set_book_genre("Книга1", "Фантастика")
+        self.assertEqual(self.collector.get_books_genre(), {"Книга1": "Фантастика"})
+
+if __name__ == '__main__':
+    unittest.main()
